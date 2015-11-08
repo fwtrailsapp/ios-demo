d3bb443ed2ba81c04f3247692b4a979e6f750b8d
Scott Weidenkopf <sweidenkopf@gmail.com>
2015-11-08 13:15:13 -0500

    Initial Commit

---
+++ ios-demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-demo
+//
+//  Created by Scott Weidenkopf on 11/8/15.
+//  Copyright © 2015 City of Fort Wayne Rivergreenways. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
